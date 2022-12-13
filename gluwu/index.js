5f9616ef5392eb8540c5f7bbaa9c8f15809d8cf7
Beef <beefers@riseup.net>
2022-12-13 20:52:14 +0000

    [Global] Update to latest gluon

--- gluwu/index.js
+++ gluwu/index.js
@@ -1,7 +1,28 @@
 import * as Gluon from "../gluon/index.js";
-import initCosmetics from "./mods/cosmetics.js";
+import logger from "./lib/logger.js";
 
 (async () => {
-    const Chromium = await Gluon.open("https://radio.uwu.network", { windowSize: [800, 800] });
-    await initCosmetics(Chromium);
+    const browser = await Gluon.open("https://radio.uwu.network", { windowSize: [800, 800], onLoad: () => {
+        document.addEventListener("DOMContentLoaded", (event) => {
+                const stylesheet = `
+                #app > div:first-child > div:first-child:not(.grid), #app > div > div:last-child > div:first-child {
+                    display: none !important;
+                }
+            
+                #app > div > div:last-child > span:nth-child(2) {
+                    visibility: hidden;
+                }
+            
+                #app > div > div:last-child > span:nth-child(4) {
+                    margin-bottom: 0.5rem;
+                }
+            `;
+
+            const style = document.createElement("style");
+            style.textContent = stylesheet;
+            document.head.append(style);
+        });
+    }});
+
+    logger.log("Ready!");
 })();
